e2ef7a60648d6721492e9b9e75eb7d293d017b9f
Mark Bojesen <bojesenmark@gmail.com>
2017-07-06 21:04:12 +0100

    initial commit

---
+++ js/app.js
@@ -0,0 +1,31 @@
+//
+//
+// /* ---------------------------------------------------------
+//                         Price calculator
+// ----------------------------------------------------------- */
+
+const priceInput = document.querySelector('[name=price]');
+const quantityInput = document.querySelector('[name=quantity]');
+const total = document.querySelector('.total');
+const quantityLabel = document.querySelector('.quantity-label')
+
+function calculatePieCost() {
+    const price = priceInput.value;
+    const quantity = quantityInput.value;
+    const cost = price * quantity;
+    total.innerText = '$' + cost.toFixed(2);
+};
+
+function updateQuantityLabel() {
+    const quantity = quantityInput.value;
+    quantityLabel.innerText = quantity;
+};
+
+calculatePieCost();
+
+priceInput.addEventListener('input', calculatePieCost);
+quantityInput.addEventListener('input', calculatePieCost);
+quantityInput.addEventListener('input', updateQuantityLabel);
+
+
+
